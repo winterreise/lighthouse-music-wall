5239e75d04954cabbb22d76c344be446c9fab2c6
winterreise <puiwing.tam@gmail.com>
2016-05-17 02:46:13 +0000

    Created users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516164312) do
+ActiveRecord::Schema.define(version: 20160517024207) do
 
   create_table "tracks", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
     t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
